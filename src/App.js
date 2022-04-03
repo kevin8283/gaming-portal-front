ec9c0a0a3b0e9b4ceb8bdff8eac59da16b404b1a
kevin <keyvinkeygen@gmail.com>
2022-04-03 11:06:06 +0200

    add detailed purchase component [DetailedPurchase]

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
 import Home from "./components/Home/Home"
 import GameContainer from "./components/Games/GameContainer"
-import PurchaseContainer from "./components/Purchases/PurchaseContainer"
+import PurchaseContainer from "./components/Purchases/Purchase/PurchaseContainer"
+import DetailedPurchase from "./components/Purchases/DetailedPurchase/DetailedPurchase"
 import ClientContainer from "./components/Clients/ClientContainer"
-import NewPurchase from './components/Purchases/NewPurchase'
+import NewPurchase from './components/Purchases/NewPurchaseForm/NewPurchase'
 import Navbar from './components/Navbar/Navbar'
 import Search from './components/Search/Search'
 import "./App.css"
@@ -24,6 +25,7 @@ export default function App() {
                   <Route path="search" element={<Search />} />
                   <Route path="games" element={<GameContainer />} />
                   <Route path="purchases" element={<PurchaseContainer />} />
+                  <Route path="purchases/:id" element={<DetailedPurchase />}/>
                   <Route path="purchases/new" element={<NewPurchase />} />
                   <Route path="clients" element={<ClientContainer />} />
                 </Route>
--- src/components/Common/Error.js
+++ src/components/Common/Error.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Alert } from '@mui/material'
 
-export default function Error({message}) {
+export default function Error({ message }) {
   return (
       <div className="common-container">
         <Alert severity="error" className="error">{message}</Alert>
